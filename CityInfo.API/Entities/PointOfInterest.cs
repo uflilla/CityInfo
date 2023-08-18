87275ca688721b6d6057568d622df5c47a148495
uflilla <uflilla@gmail.com>
2023-08-18 16:07:24 +0500

    PointOfInterest class updated to have ctor and data seeded to the database.

--- CityInfo.API/Entities/PointOfInterest.cs
+++ CityInfo.API/Entities/PointOfInterest.cs
@@ -5,6 +5,10 @@ namespace CityInfo.API.Entities;
 
 public class PointOfInterest
 {
+  public PointOfInterest(string name)
+  {
+    Name = name;
+  }
   [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
   public int Id { get; set; }
   [Required, MaxLength(50)]
